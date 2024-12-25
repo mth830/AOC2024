7bbef428aae395b93127802def3cae393143dc4a
mth830 <mth830@gmail.com>
2024-12-25 07:13:09 -0500

    cleaned up day 1-9

--- aoc6p1.js
+++ aoc6p1.js
@@ -12,30 +12,32 @@ let input =
 
 const list = input.split('\n').map(e => e.split(''));
 let dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let set = new Set();
-for (let i = 0; i < list.length; i++) {
+let visitedLocations = new Set();
+outer:for (let i = 0; i < list.length; i++) {
   for (let j = 0; j < list[0].length; j++) {
     let cur = list[i][j];
     if (cur == '^') {
       dfs(i, j);
-      break;
+      break outer;
     }
   }
 }
 
 function dfs(i, j) {
-  let dIndex = 0;
-  let inbounds = (x = i, y = j) => x >= 0 && x < list.length && y >= 0 && y < list[0].length;
+  let directionIndex = 0;
+  let inbounds = (x = i, y = j) => {
+    return x >= 0 && x < list.length && y >= 0 && y < list[0].length;
+  };
   while (true) {
-    let [a, b] = dirs[dIndex++ % 4];
+    let [a, b] = dirs[directionIndex++ % 4];
     while (inbounds(i + a, j + b) && list[i + a][j + b] !== '#') {
       i += a;
       j += b;
       if (!inbounds()) return;
-      set.add([i, j].toString());
+      visitedLocations.add([i, j].toString());
       list[i][j] = 'X';
     }
     if (!inbounds(i + a, j + b)) return;
   }
-
 }
+console.log(visitedLocations.size)
---
+++ aoc7p2.js
@@ -0,0 +1,31 @@
+let input = ``
+input = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+
+
+const lists = input
+  .split('\n')
+  .map(e => e.split(':'))
+  .map(([total, nums]) => [+total, nums.split(' ').map(Number)]);
+
+function concat(num1, num2) {
+  return Number(num1.toString() + num2.toString());
+};
+
+function dfs(i, arr, target, curr = 0) {
+  if (curr === target && i === arr.length) return true;
+  if (curr > target || i >= arr.length) return false;
+  if (dfs(i + 1, arr, target, curr + arr[i])) return true;
+  else if (dfs(i + 1, arr, target, concat(curr, arr[i]))) return true;
+  return dfs(i + 1, arr, target, curr * arr[i]);
+};
+
+const filtered = lists.filter(([total, list]) => dfs(1, list, total, list[0]));
+console.log(filtered.reduce((sum, [total, list]) => sum + total, 0));
